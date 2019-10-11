b49cf578de788b28a912397f3566b6b13bf8c565
Acer <yurahasse@gmail.com>
2019-10-11 08:53:07 +0300

    clear run()

--- src/main.java
+++ src/main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
         Board board = new Board();
-        System.out.println(board.paintSmall(12, 12));
+        System.out.println(board.paintSmall(5, 5));
     }
 
 }
